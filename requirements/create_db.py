b0b8054e9625e4ebbccf2c468ac7082c20f90759
Jan <jannaj02@protonmail.com>
2021-04-11 21:20:11 +0200

    added tables to create_db.py

--- requirements/create_db.py
+++ requirements/create_db.py
@@ -9,28 +9,29 @@ def main():
     create_table_todos()
     create_table_notes()
     create_table_profiles()
+    create_table_tags()
+    create_table_notes_tags()
     print("Finished!")
     input("Press Enter to close this window...")
 
 
 def create_table_todos():
-    sql_statement = "CREATE TABLE TASKS (" \
+    sql_statement = "CREATE TABLE tasks (" \
                     "id MEDIUMINT(9) NOT NULL AUTO_INCREMENT," \
                     "creator_id MEDIUMINT(9) NOT NULL," \
                     "text VARCHAR(64) NOT NULL," \
                     "is_done BOOLEAN NOT NULL," \
                     "creation_date DATETIME NOT NULL," \
-                    "color VARCHAR(10) DEFAULT 'Using UnChat'," \
+                    "color VARCHAR(10) DEFAULT '#8e47d1'," \
                     "PRIMARY KEY (id)," \
-                    "FOREIGN KEY (creator_id) REFERENCES PROFILES(id)" \
+                    "FOREIGN KEY (creator_id) REFERENCES profiles(id)" \
                     ")"
     cursor.execute(sql_statement)
-    print("Created 'TASKS' table successfully!")
+    print("Created 'tasks' table successfully!")
 
 
-# only a blueprint since history tables are created automatically
 def create_table_notes():
-    sql_statement = "CREATE TABLE NOTES (" \
+    sql_statement = "CREATE TABLE notes (" \
                     "id MEDIUMINT(9) NOT NULL AUTO_INCREMENT," \
                     "creator_id MEDIUMINT(9) NOT NULL" \
                     "content VARCHAR(2048) NOT NULL," \
@@ -38,14 +39,14 @@ def create_table_notes():
                     "is_highlighted BOOLEAN NOT NULL," \
                     "color VARCHAR(10) NOT NULL," \
                     "PRIMARY KEY (id)," \
-                    "FOREIGN KEY (creator_id) REFERENCES PROFILES(id)" \
+                    "FOREIGN KEY (creator_id) REFERENCES profiles(id)" \
                     ")"
     cursor.execute(sql_statement)
-    print("Created 'NOTES' table successfully!")
+    print("Created 'notes' table successfully!")
 
 
 def create_table_profiles():
-    sql_statement = "CREATE TABLE PROFILES (" \
+    sql_statement = "CREATE TABLE profiles (" \
                     "id MEDIUMINT(9) NOT NULL AUTO_INCREMENT," \
                     "name VARCHAR(16) NOT NULL," \
                     "password VARCHAR(256) NOT NULL," \
@@ -53,7 +54,28 @@ def create_table_profiles():
                     "PRIMARY KEY (id)" \
                     ")"
     cursor.execute(sql_statement)
-    print("Created 'PROFILES' table successfully!")
+    print("Created 'profiles' table successfully!")
+
+
+def create_table_tags():
+    sql_statement = "CREATE TABLE tags(" \
+                    "id MEDIUMINT(9) NOT NULL AUTO_INCREMENT," \
+                    "name VARCHAR(20) NOT NULL," \
+                    "PRIMARY KEY (id)" \
+                    ")"
+    cursor.execute(sql_statement)
+    print("Created 'tags' table successfully!")
+
+
+def create_table_notes_tags():
+    sql_statement = "CREATE TABLE tags(" \
+                    "note_id MEDIUMINT(9) NOT NULL" \
+                    "tag_id MEDIUMINT(9) NOT NULL," \
+                    "FOREIGN KEY (note_id) REFERENCES notes(id)," \
+                    "FOREIGN KEY (tag_id) REFERENCES tags(id)" \
+                    ")"
+    cursor.execute(sql_statement)
+    print("Created 'notes_tags' table successfully!")
 
 
 if __name__ == '__main__':
